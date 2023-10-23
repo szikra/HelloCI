17aa77f6a7deed260300ba4de46ef6d943791464
Szikra István <steven.spark@gmail.com>
2023-10-23 02:41:13 +0200

    +build config options to test workflow failures (compile, link, run, test)

--- source/hello_c.c
+++ source/hello_c.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+void fail2link(void);
+
 int main(void) {
 	printf("Hello C CI\r\n");
+
+#ifdef TEST_FAIL_TO_COMPILE
+	fail2compile;
+#endif
+
+#ifdef TEST_FAIL_TO_LINK
+	fail2link();
+#endif
+
+#ifdef TEST_FAIL_TO_RUN
 	*(int*)1 = 1; /// Seg Fault Runtime error Test
+#endif
+
+#ifdef TEST_FAIL_TO_TEST
+	return 1;
+#else
 	return 0;
+#endif
 }
